d72cc6f14f7bea34ccc254ce87b13aea74f6d746
jmsalvador2395 <jmsalvador2395@gmail.com>
2022-02-06 20:54:57 -0600

    trying to get the program to count words with files larger than the buffer

--- pwordcount.c
+++ pwordcount.c
@@ -25,10 +25,6 @@ void close_pipe_ends(int *fd_r, int *fd_w){
 	close(fd_w[R]);
 }
 
-void close_pipes(int *fd){
-	close(fd[R]);
-	close(fd[W]);
-}
 /*
  * code for the parent to execute
  *
@@ -51,36 +47,38 @@ void exec_parent(char *file, int *fd_r, int *fd_w){
 	//read file
 	size_t r_size;
 	printf("p1: read file\n");
-	r_size=fread(w_msg, 1, BUFF_SIZE, src_fd);
+	while(r_size=fread(w_msg, 1, BUFF_SIZE, src_fd)){
 
-	//send message buffer to p2
-	printf("p1: send data to p2\n");
-	write(fd_w[W], w_msg,strlen(w_msg)+1);
+		printf("r_size: %d\n", (int) r_size);
+		printf("from file: %s\n", w_msg);
 
+		//send message buffer to p2
+		write(fd_w[W], w_msg,r_size+1);
+		memset(w_msg, 0, BUFF_SIZE+1);
+		printf("p1: sent data to p2\n");
 
-	//read message buffer from p2
-	printf("p1: read data from p2\n");
-	read(fd_r[R], r_msg, BUFF_SIZE); 
+		//read message buffer from p2
+		read(fd_r[R], r_msg, BUFF_SIZE); 
+		printf("p1: received  data from p2\n");
 
-	int i;
+		//convert to integer
+		bufftoi(&wc, r_msg);
 
-	printf("from p2: ");
-	bufftoi(&wc, r_msg);
-	for(i=0; i<BUFF_SIZE; i++){
-		printf("%02x ", r_msg[i]);
+		//increment total
+		wc_total+=wc;
+
+		//read new words
+		printf("\n");
 	}
-	printf("\n");
-	
-	printf("wc: %d\n", wc);
-
-	wc_total+=wc;
-
 	//print word count
 	printf("p1: %d total words\n", wc_total);
 
 	printf("p1: exit\n");
+
+	//close pipes
 	fclose(src_fd);
-	//close_pipes(fd);
+	close(fd_r[R]);
+	close(fd_w[W]);
 }
 
 /*
@@ -98,28 +96,29 @@ void exec_child(int *fd_r, int *fd_w){
 	char r_msg[BUFF_SIZE+1]={0};
 
 	int i=0;
+	while(1){
+		//read data from p1
+		read(fd_r[R], r_msg, BUFF_SIZE);
+		printf("p2: received data from p1\n");
 
-	//read data from p1
-	printf("p2: read data from p1\n");
-	read(fd_r[R], r_msg, BUFF_SIZE);
+		printf("p2 r_msg: %s\n", r_msg);
+		//count words
+		wc=count_words(r_msg, strlen(r_msg));
+		memset(r_msg, 0, BUFF_SIZE);
+		printf("p2: counted words\n");
 
-	//count words
-	printf("p2: counting words\n");
-	wc=count_words(r_msg, strlen(r_msg));
+		itobuff(w_msg, wc);
 
-	itobuff(w_msg, wc);
-
-	printf("to p1: ");
-	for(i=0;i<4;i++){
-		printf("%02x ", w_msg[i]);
+		//send word count back to p1
+		write(fd_w[W], w_msg, sizeof(int)+1);
+		printf("p2: sent result back to p1\n");
 	}
-	printf("\n");
 
-	//send word count back to p1
-	printf("p2: send result back to p1\n");
-	write(fd_w[W], w_msg, sizeof(int)+1);
+	printf("p2: exit");
 	
-	//close_pipes(fd);
+	//close pipes
+	close(fd_r[R]);
+	close(fd_w[W]);
 }
 
 void itobuff(char *buff, int src){
@@ -130,37 +129,11 @@ void itobuff(char *buff, int src){
 }
 
 void bufftoi(int *dest, char *buff){
-	printf("bufftoi\n");
 	int i;
 	for(i=0; i<4; i++){
 		*dest<<=8;
 		*dest=buff[i]&0xFF;
 	}
-}
-
-void itoa(char *buff, unsigned int src){
-
-	int temp=src;
-	int power=1;
-
-	while(power<temp)
-		power*=10;
-	power/=10;
-
-	char digit;
-	int i;
-	while(temp != 0){
-		digit= ((temp/power)&0xFF) + 0x30;
-		printf("%d ", digit);
-		//buff[i]=digit;
-
-		i+=1;
-		if(digit!=0)
-			temp=temp-digit*power;
-		if(power!=1)
-			power/=10;
-	}
-	printf("\n");
 }
 
 int count_words(char *msg, int msg_len){
